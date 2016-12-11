045a73820a81de2240e5810c758f463cde81d8e2
schatzmana <schatzmana@gmail.com>
2016-12-10 20:32:56 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 83ed347ee6e4e89d8fe275a1cd8dfae90b1aac48, reversing
    changes made to 01aeaaad747f637493dca740a32c638f4fbb578e.

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect('classroom.cs.unc.edu', 
-		      'aburaja', 
-                      'aburaja', 
-		      'aburajadb');
-?>
--- viewbycategory.php
+++ viewbycategory.php
@@ -2,10 +2,7 @@
 
 
 $category = $_GET['category'];
-?>
-<h1><?php echo "".$category."" ?></h1>
 
-<?php
 $servername = "classroom.cs.unc.edu";
 $username = "psanka";
 $password = "random123";
@@ -30,54 +27,28 @@ $categoryid;
     }
  $sql = "SELECT * FROM Posts p WHERE p.CategoryID =".$categoryid;
     echo "I made it here";
+echo $_GET['id'];
      $result = $conn->query($sql);
 
-if ($result->num_rows > 0): ?>
-<table>
-    <tr>
-        <td>Title</td>
-        <td>Description</td>
-        <td>Price</td>
-        <td>Phone Number</td>
-        <td>Username</td>
-        <td>Picture</td>
-        
-    </tr>
-    
-       <?php while($row = $result->fetch_assoc()): ?> 
-    <tr>
-        <td><a href="<?php print ("viewpost.php?id=".$row["PostID"]."")?>"><?php echo "".$row["Title"].""; ?></a></td>
-        <td><?php echo "" . $row["Description"].""; ?></td>
-        <td><?php echo "" . $row["Price"].""; ?></td>
-        <td><?php echo "" . $row["PhoneNumber"].""; ?></td>
-        <td><?php 
-             $picture = $row["Picture"];
-             $userID = $row["UserID"];
-             $sql = "SELECT UserName FROM Users WHERE UserID = '".$userID ."'";
-    $user_name;
-     $userresult = $conn->query($sql);
-        while($row = $userresult->fetch_assoc()) {
-        $user_name = $row["UserName"];
-    }
-        echo "" . $user_name.""; ?></td>
-        <td><img src="<?php echo "" . $picture.""; ?>"></td>
-        
-    </tr>
-    <?php endwhile; ?>
-</table>
-<?php endif;
-
+if ($result->num_rows > 0) {
     // output data of each row
-   // while($row = $result->fetch_assoc()) {
-     //   echo "title " . $row["Title"]."";
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Title"]."";
+        $title = $row["Title"];
+        $description = $row["Description"];
+        $price = $row["Price"];
+        
+    }
+    
+    }
 
-//        $title = $row["Title"];
-//        $description = $row["Description"];
-//        $price = $row["Price"];
-//        
-//    }
-//    
-//    }
+echo "<b>
+<center>Database Output</center>
+</b>
+<br>
+<br>";
 
-
+echo "<b>
+$title-name $description-name2</b>
+<br>";
     ?>
