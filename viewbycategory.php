f94725225ef388c842de32e4d9a7e33a8f74534b
pranavisanka <pranavisanka@gmail.com>
2016-12-10 19:08:13 -0500

    view pages

--- viewbycategory.php
+++ viewbycategory.php
@@ -2,7 +2,10 @@
 
 
 $category = $_GET['category'];
+?>
+<h1><?php echo "".$category."" ?></h1>
 
+<?php
 $servername = "classroom.cs.unc.edu";
 $username = "psanka";
 $password = "random123";
@@ -30,25 +33,52 @@ $categoryid;
 echo $_GET['id'];
      $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["Title"]."";
-        $title = $row["Title"];
-        $description = $row["Description"];
-        $price = $row["Price"];
+if ($result->num_rows > 0): ?>
+<table>
+    <tr>
+        <td>Title</td>
+        <td>Description</td>
+        <td>Price</td>
+        <td>Phone Number</td>
+        <td>Username</td>
+        <td>Picture</td>
         
-    }
+    </tr>
     
+       <?php while($row = $result->fetch_assoc()): ?> 
+    <tr>
+        <td><a href="<?php print ("viewpost.php?id=".$row["PostID"]."")?>"><?php echo "".$row["Title"].""; ?></a></td>
+        <td><?php echo "" . $row["Description"].""; ?></td>
+        <td><?php echo "" . $row["Price"].""; ?></td>
+        <td><?php echo "" . $row["Phone Number"].""; ?></td>
+        <td><?php 
+            
+             $userID = $row["UserID"];
+             $sql = "SELECT UserName FROM Users WHERE UserID = '".$userID ."'";
+    $user_name;
+     $userresult = $conn->query($sql);
+        while($row = $userresult->fetch_assoc()) {
+        $user_name = $row["UserName"];
     }
+        echo "" . $user_name.""; ?></td>
+        <td><?php echo "" . $row["Picture"].""; ?></td>
+        
+    </tr>
+    <?php endwhile; ?>
+</table>
+<?php endif;
 
-echo "<b>
-<center>Database Output</center>
-</b>
-<br>
-<br>";
+    // output data of each row
+   // while($row = $result->fetch_assoc()) {
+     //   echo "title " . $row["Title"]."";
 
-echo "<b>
-$title-name $description-name2</b>
-<br>";
+//        $title = $row["Title"];
+//        $description = $row["Description"];
+//        $price = $row["Price"];
+//        
+//    }
+//    
+//    }
+
+
     ?>
